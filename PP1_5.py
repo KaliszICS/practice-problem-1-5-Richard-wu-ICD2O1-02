1cd8c30d65adac3118e77dcfa2569decd2ecef95
Richard-wu-ICD2O1-02 <Richard.wu4@student.tdsb.on.ca>
2024-11-05 19:51:31 -0500

    Update PP1_5.py

--- PP1_5.py
+++ PP1_5.py
@@ -1,24 +1,27 @@
-
 def q1():
   #Write Assignment code here
-
+  a = input("Input an integer: ")
+  a = int(a)
+  print(a + 3)
 def q2():
   #Write Assignment code here
-
+  user1 = input("Input a number: ")
+  a = str(user1) + "4"
+  a = float(a)+2
+  print(a)
 def q3():
   #Write Assignment code here
-
+  radius = float(input("Input a radius: "))
+  area = float((radius**2)*3.14)
+  print(area)
 def q4():
   #Write Assignment code here
+  user5 = float(input("Input a number: "))
+  t = int(user5 * 12)
+  print(t)
 
 def q5():
   #Write Assignment code here
-
-#Comment this code out when running tests
-#Do not comment this out when running your program normally
-
-q1()
-q2()
-q3()
-q4()
-q5()
+  user5 = (input("Input an integer: "))
+  result = int(user5) + 5
+  print(f'Your number + 5 is {result}')
